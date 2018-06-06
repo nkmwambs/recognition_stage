bb1e744b5ab28865f1edcaf8690d1c745e196637
Nicodemus Karisa <NKarisa@CI.ORG>
2018-06-06 16:20:16 +0300

    More work

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -21,7 +21,7 @@ class Dashboard extends CI_Controller
         $this->load->library('session');
 		
 		/** System Feature Session Tag **/
-		$this->session->set_userdata('view_type', get_called_class());
+		$this->session->set_userdata('view_type', "dashboard");
 		
        /*cache control*/
 		$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
@@ -36,7 +36,7 @@ class Dashboard extends CI_Controller
             redirect(base_url(), 'refresh');
 			
         $page_data['page_name']  = "dashboard";
-        $page_data['view_type']  = get_called_class();
+        $page_data['view_type']  = "dashboard";
         $page_data['page_title'] = get_phrase('dashboard');
         $this->load->view('backend/index', $page_data);
     }
@@ -48,7 +48,7 @@ class Dashboard extends CI_Controller
             redirect(base_url(), 'refresh');
 			
         $page_data['page_name']  = __FUNCTION__;
-        $page_data['view_type']  = "navigation";
+        $page_data['view_type']  = "dashboard";
         $page_data['page_title'] = get_phrase('dashboard');
         $this->load->view('backend/index', $page_data);
     }
