722708310f00aafed37270106dc16bcc6a15a125
Nicodemus Karisa <NKarisa@CI.ORG>
2018-06-11 06:37:35 +0300

    Removed test files

--- application/views/backend/navigation.php
+++ application/views/backend/navigation.php
@@ -24,7 +24,7 @@
         </div>
     </header>
 
-    <div style=""></div>	
+    <div style=""></div>
     <ul id="main-menu" class="">
         <!-- add class "multiple-expanded" to allow multiple submenus to open -->
         <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
@@ -37,8 +37,8 @@
                 <span><?php echo get_phrase('dashboard'); ?></span>
             </a>
         </li>
-        
-        
+
+
         <!-- SURVEYS -->
         <li class="<?php if ($page_name == 'survey_results'|| $page_name == 'votes'  ||  $page_name == 'nominate' || $page_name == 'manage_surveys' || $page_name == 'category_groups' || $page_name == 'categories'|| $page_name == 'survey_setting') echo 'opened active';?>">
             <a href="#">
@@ -46,61 +46,61 @@
                 <span><?php echo get_phrase('surveys'); ?></span>
             </a>
             <ul>
-            	
+
 	            <li class="manage_surveys <?php if ($page_name == 'category_groups' || $page_name == 'votes'  || $page_name == 'categories'|| $page_name == 'survey_setting' ) echo 'opened active';?>">
 	               <a href="#" class="ajax-content">
 	                   <span><i class="entypo-tools"></i> <?php echo get_phrase('manage_surveys'); ?></span>
 	                    </a>
-	                    
-	                    
+
+
 	                    <ul>
 	                        <li class="manage_grouping <?php if ($page_name == 'category_groups') echo 'active'; ?>">
 	                        	<a href="<?php echo base_url(); ?>surveys/category_groups" class="">
 	                                <span><i class="entypo-logo-db"></i><?php echo get_phrase('category_groups'); ?></span>
 	                            </a>
 	                        </li>
-	                        
+
 	                        <li class="manage_category <?php if ($page_name == 'categories') echo 'active'; ?>">
 	                        	<a href="<?php echo base_url(); ?>surveys/categories" class="">
 	                                <span><i class="entypo-layout"></i><?php echo get_phrase('categories'); ?></span>
 	                            </a>
 	                        </li>
-	                        
+
 	                        <li class="survey_settings <?php if ($page_name == 'survey_setting') echo 'active'; ?>">
 	                        	<a href="<?php echo base_url(); ?>surveys/survey_setting" class="">
 	                                <span><i class="entypo-cog"></i><?php echo get_phrase('survey_setting'); ?></span>
 	                            </a>
 	                        </li>
-	                        
+
 	                         <li class="reset_vote <?php if ($page_name == 'votes') echo 'active'; ?>">
 	                        	<a href="<?php echo base_url(); ?>surveys/votes" class="">
 	                                <span><i class="entypo-star"></i><?php echo get_phrase('votes'); ?></span>
 	                            </a>
 	                        </li>
-	                        
-	                        
-	                        
+
+
+
                         </ul>
-	                    
+
 	            </li>
-                
+
 
 	            <li class="<?php if ($page_name == 'nominate') echo 'active'; ?> ">
 	                    <a href="<?php echo base_url("surveys/nominate"); ?>" class="ajax-content">
 	                        <span><i class="entypo-compass"></i> <?php echo get_phrase('nominate'); ?></span>
 	                    </a>
 	            </li>
-	            
+
 	            <!-- <li class="survey_results <?php if ($page_name == 'survey_results') echo 'active'; ?> ">
 	                    <a href="<?php echo base_url("surveys/survey_results"); ?>" class="ajax-content">
 	                        <span><i class="entypo-doc-text"></i> <?php echo get_phrase('survey_results'); ?></span>
 	                    </a>
 	            </li> -->
-                
+
             </ul>
         </li>
-        
-        
+
+
         <!-- MESSAGES -->
         <li class="messages <?php if ($page_name == 'messages') echo 'active'; ?> ">
             <a href="<?php echo base_url();?>messages/message">
@@ -108,10 +108,10 @@
                 <span><?php echo get_phrase('messages'); ?></span>
             </a>
         </li>
-        
-        
+
+
         <!-- SETTINGS -->
-        
+
         <li class="manage_settings <?php
         if ($page_name == 'system_settings' ||
                 $page_name == 'manage_language' ||
@@ -130,7 +130,7 @@
 	                    </a>
 	                </li>
 
-                
+
 	                <li class="sms_settings <?php if ($page_name == 'sms_settings') echo 'active'; ?> ">
 	                    <a href="<?php echo base_url("settings/sms_settings"); ?>" class="ajax-content">
 	                        <span><i class="entypo-mobile"></i> <?php echo get_phrase('sms_settings'); ?></span>
@@ -150,14 +150,14 @@
 
 		<!-- SETTINGS -->
         <li class="manage_accounts <?php
-        if ($page_name == 'countries' || 
-            $page_name == 'roles'|| 
+        if ($page_name == 'countries' ||
+            $page_name == 'roles'||
             $page_name == 'departments' ||
             $page_name == 'teams' ||
 			$page_name == 'profiles' ||
-			$page_name == 'manage_users' || 
+			$page_name == 'manage_users' ||
 			$page_name == 'mail_templates')
-			
+
             echo 'opened active';
         ?> ">
             <a href="#">
@@ -165,16 +165,16 @@
                 <span><?php echo get_phrase('account'); ?></span>
             </a>
             <ul>
-                               
+
                 <!--ACCOUNT SET UP PARAMETERS-->
-                <li class="manage_setup_parameters <?php 
-                		if ($page_name == 'countries' || 
-                			$page_name == 'roles'|| 
+                <li class="manage_setup_parameters <?php
+                		if ($page_name == 'countries' ||
+                			$page_name == 'roles'||
                 			$page_name == 'departments' ||
                 			$page_name == 'teams' ||
-							$page_name == 'profiles' || 
-							$page_name == 'mail_templates') 
-                			
+							$page_name == 'profiles' ||
+							$page_name == 'mail_templates')
+
                 			echo 'opened active'; ?> ">
                     <a href="#">
                         <span><i class="entypo-loop"></i> <?php echo get_phrase('setup'); ?></span>
@@ -185,57 +185,51 @@
                                 <span><i class="entypo-globe"></i><?php echo get_phrase('countries'); ?></span>
                             </a>
                         </li>
-                                              
+
                         <li class="setup_departments <?php if ($page_name == 'departments') echo 'active'; ?>">
                         	<a href="<?php echo base_url(); ?>account/departments"  class="">
                                     <span><i class="entypo-progress-3"></i><?php echo get_phrase('departments'); ?></span>
                             </a>
                         </li>
-                        
+
                         <li class="setup_teams <?php if ($page_name == 'teams') echo 'active'; ?>">
                         	<a href="<?php echo base_url(); ?>account/teams"  class="">
                                     <span><i class="entypo-users"></i><?php echo get_phrase('teams'); ?></span>
                             </a>
                         </li>
-                        
+
                          <li class="setup_roles <?php if ($page_name == 'roles') echo 'active'; ?>">
                         	<a href="<?php echo base_url(); ?>account/roles"  class="">
                                     <span><i class="entypo-bag"></i><?php echo get_phrase('roles'); ?></span>
                             </a>
                         </li>
-                        
+
                         <li class="setup_profiles <?php if ($page_name == 'profiles') echo 'active'; ?>">
                         	<a href="<?php echo base_url(); ?>account/profiles"  class="">
                                     <span><i class="entypo-tag"></i><?php echo get_phrase('profiles'); ?></span>
                             </a>
                         </li>
-                        
+
                         <li class="mail_templates <?php if ($page_name == 'mail_templates') echo 'active'; ?>">
 	                        	<a href="<?php echo base_url(); ?>account/mail_templates" class="">
 	                                <span><i class="entypo-popup"></i><?php echo get_phrase('mail_templates'); ?></span>
 	                            </a>
 	                    </li>
-                        
+
                     </ul>
                 </li>
-                
-                
+
+
                 <li class="manage_users <?php if ($page_name == 'manage_users') echo 'active'; ?> ">
                     <a href="<?php echo base_url("account/manage_users"); ?>">
                         <span><i class="entypo-users"></i> <?php echo get_phrase('manage_users'); ?></span>
                     </a>
                 </li>
-                         
+
             </ul>
         </li>
-		
-           <li class="dashboard <?php if ($page_name == 'testing') echo 'active'; ?> ">
-            <a href="<?php echo base_url();?>dashboard/testing">
-                <i class="entypo-gauge"></i>
-                <span><?php echo "Test"; ?></span>
-            </a>
-        </li>
 
+           
     </ul>
 
 </div>
