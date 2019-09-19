c15958e24d53e53cc1a2699b69280addc85a1ccd
nkmwambs <nkmwambs@gmail.com>
2019-09-19 13:32:59 +0300

    Show error when user data has mismatch - Updated

--- application/views/backend/sso_error_message.php
+++ application/views/backend/sso_error_message.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div class="row">
-  <div class="col-xs-12 text-center">
-    <span>Error occurred! There is a mismatch of credentials and therefore you can't be authenticated. Please contact your HRBP.</span>
+  <div class="col-xs-12" style="text-align:center;">
+    <span style="border:1px orange solid;border-radius:5px;position:absolute;top:0px;bottom:0px;">Error occurred! There is a mismatch of credentials and therefore you can't be authenticated. Please contact your HRBP.</span>
   </div>
 <div>
